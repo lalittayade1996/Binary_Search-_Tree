957e3e0ac4287989db03c8b58dfd71a12c9f32a6
lalittayade1996 <lalittayade2.9.1996@gmail.com>
2021-06-28 02:20:47 +0530

    Added : Welcome To Binary Search Tree Problems

---
+++ src/main/java/com/bridgelabz/bst/BinarySearchTree_Project/Welcome.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.bst.BinarySearchTree_Project;
+
+public class Welcome {
+	public static void main(String[] args) {
+		System.out.println("Welcome To Binary Search Tree Problems");
+	}
+}
